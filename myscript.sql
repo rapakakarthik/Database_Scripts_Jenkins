6328d349fd462865bc52dec61fa7420e6e74267d
Rapaka Karthik <rapakakarthik7@gmail.com>
2025-01-06 11:43:48 +0530

    num

--- myscript.sql
+++ myscript.sql
@@ -1,3 +1,7 @@
+-- Add mobile column if it does not exist
+ALTER TABLE devera ADD COLUMN IF NOT EXISTS mobile VARCHAR(15);
+
+-- Update the user with new values
 UPDATE devera 
-SET usermail = 'dakumaharaj', mail = 'daku@example.com' 
+SET usermail = 'dakumaharaj', mail = 'daku@example.com', mobile = '1234567890' 
 WHERE usermail = 'gamechanger';
